bb043533d26cf39fe7ea7b37f9cbe3aa47afe17b
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-04-02 01:46:51 +0100

    Refactored code, all known bugs have been fixed such as looping problems.

--- distraction_shield.py
+++ distraction_shield.py
@@ -9,66 +9,78 @@ import webbrowser
 
 
 class AppBlocker:
-
     def __init__(self, root):
 
         root.title("Distraction Shield")
-        root.iconbitmap('assets/logo.ico')
+        root.iconbitmap("assets/logo.ico")
         root.resizable(False, False)
         root.minsize(500, 200)
         self.run = True
 
         basic_font = Font(
-            family='roboto',
+            family="roboto",
             size=12,
-            weight='normal',
-            slant='roman',
+            weight="normal",
+            slant="roman",
             underline=0,
-            overstrike=0
+            overstrike=0,
         )
 
         alert_font = Font(
-            family='roboto',
+            family="roboto",
             size=10,
-            weight='normal',
-            slant='roman',
+            weight="normal",
+            slant="roman",
             underline=0,
-            overstrike=0
+            overstrike=0,
         )
 
         # TTK Style elements
-        bg = '#333a3b'
+        bg = "#333a3b"  # Background colour
         fstyle = ttk.Style()
-        fstyle.configure('Frame.TFrame', background=bg)
+        fstyle.configure("Frame.TFrame", background=bg)
         lstyle = ttk.Style()
-        lstyle.configure('Label.TLabel', foreground='white',
-                         background=bg)
+        lstyle.configure("Label.TLabel", foreground="white", background=bg)
 
-        master_frame = ttk.Frame(root, padding='.2i', style='Frame.TFrame')
+        master_frame = ttk.Frame(root, padding=".2i", style="Frame.TFrame")
         master_frame.grid(row=0, column=0)
-        btn_frame = ttk.Frame(master_frame, style='Frame.TFrame')
+        btn_frame = ttk.Frame(master_frame, style="Frame.TFrame")
         btn_frame.grid(row=3, column=0, columnspan=3)
 
         newprocess_lbl = ttk.Label(
-            master_frame, text="Enter process to block:", font=basic_font, width=26, style='Label.TLabel')
+            master_frame,
+            text="Enter process to block:",
+            font=basic_font,
+            width=26,
+            style="Label.TLabel",
+        )
         self.newprocess_inp = ttk.Entry(master_frame, width=30)
-        add_btn = ttk.Button(master_frame, text='Add',
-                             command=self.add_process)
+        add_btn = ttk.Button(master_frame, text="Add", command=self.add_process)
         remprocess_lbl = ttk.Label(
-            master_frame, text="Remove process from blocked:", font=basic_font, width=26, style='Label.TLabel')
+            master_frame,
+            text="Remove process from blocked:",
+            font=basic_font,
+            width=26,
+            style="Label.TLabel",
+        )
         self.remprocess_inp = ttk.Entry(master_frame, width=30)
-        del_btn = ttk.Button(master_frame, text='Remove',
-                             command=self.remove_process)
-        empty_btn = ttk.Button(
-            btn_frame, text='Empty List', command=self.clear_list)
-        show_btn = ttk.Button(btn_frame, text='View List',
-                              command=self.view_list)
-        run_btn = ttk.Button(btn_frame, text='Run Blocker', command=lambda: [
-                             self.set_true(), self.run_blocker()])
-        stop_btn = ttk.Button(
-            btn_frame, text='Stop Blocker', command=self.stop_blocker)
+        del_btn = ttk.Button(master_frame, text="Remove", command=self.remove_process)
+        empty_btn = ttk.Button(btn_frame, text="Empty List", command=self.clear_list)
+        show_btn = ttk.Button(btn_frame, text="View List", command=self.view_list)
+        run_btn = ttk.Button(
+            btn_frame,
+            text="Run Blocker",
+            command=lambda: [self.set_true(), self.run_blocker()],
+        )
+        stop_btn = ttk.Button(btn_frame, text="Stop Blocker", command=self.stop_blocker)
         self.status_lbl = ttk.Label(
-            master_frame, text="", width=70, font=alert_font, foreground='#15DB00', style='Label.TLabel')
+            master_frame,
+            text="",
+            width=70,
+            font=alert_font,
+            foreground="#15DB00",
+            style="Label.TLabel",
+        )
 
         newprocess_lbl.grid(row=1, column=0, pady=(0, 20))
         self.newprocess_inp.grid(row=1, column=1, pady=(0, 20))
@@ -76,83 +88,78 @@ class AppBlocker:
         remprocess_lbl.grid(row=2, column=0, pady=(0, 20))
         self.remprocess_inp.grid(row=2, column=1, pady=(0, 20))
         del_btn.grid(row=2, column=2, pady=(0, 20), padx=(20, 0))
-
         show_btn.grid(row=0, column=0, padx=(0, 10), pady=(0, 10))
         run_btn.grid(row=0, column=1, padx=(10, 0), pady=(0, 10))
         empty_btn.grid(row=1, column=0, padx=(0, 10))
         stop_btn.grid(row=1, column=1, padx=(10, 0))
-
-        self.status_lbl.grid(row=5, column=0, columnspan=3,
-                             pady=(20, 0), sticky='W')
+        self.status_lbl.grid(row=5, column=0, columnspan=3, pady=(20, 0), sticky="W")
 
     def alert_message(self, alert_txt):
-        self.status_lbl['text'] = ""
-        self.status_lbl['text'] = alert_txt
+        self.status_lbl["text"] = ""
+        self.status_lbl["text"] = alert_txt
 
     def add_process(self):
-        # Append user entry to blocked_apps.txt file and clear entry field
+        """Append user entry to blocked_apps.txt file and clear entry field"""
         entry_txt = self.newprocess_inp.get()
 
         if len(entry_txt) == 0:
             self.alert_message(
-                "Input an application or process you want to block and click add. eg: 'steam.exe'")
-
+                "Input an application or process you want to block and click add. eg: 'steam.exe'"
+            )
         else:
-            with open('blocked_apps.txt', 'a') as file:
+            with open("blocked_apps.txt", "a") as file:
                 file.write(f"{entry_txt}\n")
                 self.alert_message(f"Added {entry_txt} to block list.")
 
-            self.newprocess_inp.delete(0, 'end')
+            self.newprocess_inp.delete(0, "end")
 
     def remove_process(self):
-        # Removes a process or app from the block list
+        """Removes a process or app from the block list"""
         remove_txt = self.remprocess_inp.get()
 
         if len(remove_txt) == 0:
-            self.alert_message(f"Please enter an application or process that is in the block list.")
+            self.alert_message(
+                f"Please enter an application or process that is in the block list."
+            )
         else:
-            with open('blocked_apps.txt', 'r') as file:
+            with open("blocked_apps.txt", "r") as file:
                 apps = file.readlines()
-
-            with open('blocked_apps.txt', 'w') as new_file:
+            with open("blocked_apps.txt", "w") as new_file:
                 for app in apps:
                     if app.strip("\n") != remove_txt:
                         new_file.write(app)
-
             self.alert_message(f"{remove_txt} removed from block list.")
-        self.remprocess_inp.delete(0, 'end')
+        self.remprocess_inp.delete(0, "end")
 
     def view_list(self):
         webbrowser.open("blocked_apps.txt")
 
     def clear_list(self):
-        with open('blocked_apps.txt', 'w'):
+        with open("blocked_apps.txt", "w"):
             pass
         self.alert_message("All items in block list have been deleted.")
 
     def set_true(self):
-        # Sets run to true so that run_blocker does not infinite loop
+        """Sets run to true so that run_blocker does not infinite loop"""
         self.run = True
 
     def run_blocker(self):
-        # Kills all task processes specified within the blocked_apps.txt file
+        """Kills all task processes specified within the blocked_apps.txt file"""
 
-        with open('blocked_apps.txt', 'r') as apps:
-            app_list = [i.replace('\n', "") for i in apps.readlines()]
+        with open("blocked_apps.txt", "r") as apps:
+            app_list = [i.replace("\n", "") for i in apps.readlines()]
 
         if app_list == []:
-            self.alert_message(
-                "Please add an application or process to block.")
+            self.alert_message("Please add an application or process to block.")
 
         elif self.run:
             self.alert_message("Blocker activated!")
-
             for app in app_list:
                 subprocess.call(f"taskkill /IM {app} /F", shell=True)
             root.after(5000, self.run_blocker)
 
     def stop_blocker(self):
-        # Stops the blocker from completing another loop
+        """Stops the blocker from completing another loop"""
         if self.run == True:
             self.run = False
             self.alert_message("Process blocker deactivated.")
